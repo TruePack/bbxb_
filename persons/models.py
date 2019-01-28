83c63acb17681c998da9f130ea99c8ed2cf3a56a
TruePack <shnayder_a@outlook.com>
2019-01-28 22:22:29 +0700

    Created Person model with model's defs (validation, url etc.)

--- persons/models.py
+++ persons/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+from datetime import date
+
+
+class Person(models.Model):
+    """
+    Class defining a Person's model.
+    """
+
+    MALE = "Male"
+    FEMALE = "Female"
+    OTHER = "Other"
+
+    # Choices for gender field.
+    GENDER = (
+        (MALE, "Male"),
+        (FEMALE, "Female"),
+        (OTHER, "Other")
+    )
+
+    # Validator for phone number.
+    phone_regex = RegexValidator(
+        regex=r"^\+\d{11}$",
+        message="Phone number must be entered in the format: +999999999.",
+    )
+    # Validator for full name. Format is Last First Middle names.
+    full_name_regex = RegexValidator(
+        regex=r"^[A-Z][a-z]+\s[A-Z][a-z]+\s[A-Z][a-z]+(?:vich|vna)$",
+        message="Enter you 'Last First Middle' names.",
+    )
+
+    full_name = models.CharField(max_length=60,
+                                 validators=[full_name_regex],
+                                 help_text="Ivanov Ivan Ivanovich")
+
+    date_of_birthday = models.DateField(help_text="YYYY-MM-DD")
+    gender = models.CharField(max_length=6, choices=GENDER)
+    telephone_number = models.CharField(max_length=12,
+                                        validators=[phone_regex],
+                                        help_text="+99999999999")
+    start_date_study = models.DateField(help_text="YYYY-MM-DD")
+    end_date_study = models.DateField(help_text="YYYY-MM-DD")
+    study_group = models.CharField(max_length=20,
+                                   help_text="Group name, for ex. '25/17'")
+    educational_institution = models.CharField(max_length=20,
+                                               help_text="For ex.: 'MSU'")
+
+    class Meta:
+        ordering = ["full_name", "date_of_birthday"]
+        verbose_name = "Person"
+        verbose_name_plural = "Persons"
+
+    def __str__(self):
+        """
+        String for representing the Person object (in Admin site etc.)
+        """
+        return self.full_name
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of Person.
+        """
+        return f"/persons/person_id_{self.id}"
+
+    def clean(self):
+        """
+        Primitive Date-fields validation. Warns on obvious mistakes with
+        some tips.
+        """
+        if self.start_date_study > self.end_date_study:
+            raise ValidationError("Start date cannot precede end date")
+        if self.start_date_study > date.today():
+            raise ValidationError("Date cant be in future")
+        if self.start_date_study < self.date_of_birthday:
+            raise ValidationError("You cant start study before birth. :^)")
+
