3a9084490f00542416c538e2fb5a173b98068dce
TruePack <shnayder_a@outlook.com>
2019-01-28 23:23:52 +0700

    Update urls for edit and create persons views

--- BBXB/urls.py
+++ BBXB/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', views.PersonListView.as_view(), name="person_full"),
     path('persons/person_id_<int:pk>/', views.PersonDetailView.as_view(),
          name="person_detail"),
+    path('person/new/', views.person_new, name="person_new"),
+    path('persons/person_id_<int:pk>/edit', views.person_edit,
+         name='person_edit'),
 ]
