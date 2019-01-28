25a106b844efa638c23add4c47ed58afcbe83b15
TruePack <shnayder_a@outlook.com>
2019-01-28 22:24:49 +0700

    Register models for admin-panel

--- persons/admin.py
+++ persons/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Person, Document
 
-# Register your models here.
+admin.site.register(Person)  # Register User model to Django Admin.
+admin.site.register(Document)  # Register Document model to Django Admin.
