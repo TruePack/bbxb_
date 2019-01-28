1443d925f63db6683c3a3cd72fc233ea229588a3
TruePack <shnayder_a@outlook.com>
2019-01-28 23:03:10 +0700

    Created ListView for Person's model

--- persons/views.py
+++ persons/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Document, Person
+
+
+class PersonListView(ListView):
+    """
+    ListView for Person`s model
+
+    Return querysets all persons with related document (passport).
+    """
+
+    model = Person
+    template_name = "persons/person_full.html"
+    context_object_name = 'persons'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["documents"] = Document.objects.filter(
+            type_of_document="Passport")
+        return context
